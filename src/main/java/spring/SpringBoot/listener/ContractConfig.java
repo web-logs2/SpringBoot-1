2a6ff21a2ab1511c89c790ab914b33a9788173f6
李晓康 <lixiaokang05@meituan.com>
2023-05-07 17:30:25 +0800

    修正代码

--- src/main/java/spring/SpringBoot/listener/ContractConfig.java
+++ src/main/java/spring/SpringBoot/listener/ContractConfig.java
@@ -1,28 +1,20 @@
 package spring.SpringBoot.listener;
 
-import com.sun.deploy.trace.Trace;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
-import org.web3j.abi.EventEncoder;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.Request;
-import org.web3j.protocol.core.methods.response.EthBlockNumber;
 import org.web3j.protocol.core.methods.request.EthFilter;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.tx.ClientTransactionManager;
-import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.StaticGasProvider;
 import spring.SpringBoot.solidity.NRaffleFactory;
 
 import java.io.IOException;
 import java.math.BigInteger;
-
 
 /**
  * 智能合約
@@ -31,13 +23,12 @@ import java.math.BigInteger;
 public class ContractConfig {
 
     //智能合约部署地址
-    private String contractAddress = "0xC56104e2108CBC57720Cb58eCd8000e57F25C7f8";
+    private String contractAddress = "0x05896f5A26b44929a85B5445C876f1CB3948E0B8";
 
     @Bean
     @Scope("prototype")
     public Web3j web3j() {
         return Web3j.build(new HttpService("https://sepolia.infura.io/v3/3a4cf0ed857e458f8a704efd8211a336"));
-
     }
 
     @Bean
@@ -48,35 +39,27 @@ public class ContractConfig {
             Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
             String clientVersion = web3ClientVersion.getWeb3ClientVersion();
             System.out.println("clientVersion" + clientVersion);
-            // 以某个用户的身份调用合约
-            TransactionManager transactionManager = new ClientTransactionManager(web3j, "acdf03d0669bbd6191f79dfd224a0cd3a0b7d50df59fda874e48cc35d4a5619e");
             //加载智能合约
-
             BigInteger gasPrice = web3j.ethGasPrice().send().getGasPrice();
             Credentials credentials = Credentials.create("acdf03d0669bbd6191f79dfd224a0cd3a0b7d50df59fda874e48cc35d4a5619e");
 
             nraffleFactory = NRaffleFactory.load(contractAddress, web3j, credentials,
-                new StaticGasProvider(gasPrice, BigInteger.valueOf(3000000L)));
+                    new StaticGasProvider(gasPrice, BigInteger.valueOf(3000000L)));
             return nraffleFactory;
-
         } catch (Exception e) {
             e.printStackTrace();
             throw e;
-            //throw new RRException("连接智能合约异常");
         }
     }
 
     @Bean
-    //监听这里才用每次都生成一个新的对象，因为同时监听多个事件不能使用同一个实例
+    //监听这里采用每次都生成一个新的对象，因为同时监听多个事件不能使用同一个实例
     @Scope("prototype")
     @Autowired
-    public EthFilter ethFilter(NRaffleFactory trace) throws IOException {
+    public EthFilter ethFilter(NRaffleFactory trace) {
         //获取启动时监听的区块
         return new EthFilter(DefaultBlockParameterName.EARLIEST,
                 DefaultBlockParameterName.LATEST,
                 trace.getContractAddress());
     }
-
-
-
 }
--- src/main/java/spring/SpringBoot/listener/ServiceRunner.java
+++ src/main/java/spring/SpringBoot/listener/ServiceRunner.java
@@ -7,23 +7,16 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.web3j.abi.EventEncoder;
-import org.web3j.abi.EventValues;
 import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Event;
 import org.web3j.abi.datatypes.Type;
-import org.web3j.abi.datatypes.generated.Bytes32;
-import org.web3j.abi.datatypes.generated.Uint256;
-import org.web3j.abi.datatypes.generated.Uint32;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.request.EthFilter;
 
-
 import java.util.Arrays;
 import java.util.List;
-
-import static org.web3j.tx.Contract.staticExtractEventParameters;
 
 /**
  * 服务监听器，继承ApplicationRunner，在spring启动时启动
@@ -37,7 +30,6 @@ public class ServiceRunner implements ApplicationRunner {
      */
     private Logger log = LoggerFactory.getLogger(ServiceRunner.class);
 
-
     @Autowired
     private Web3j web3j;
 
@@ -46,28 +38,21 @@ public class ServiceRunner implements ApplicationRunner {
     private EthFilter ethFilter;
 
     @Override
-    public void run(ApplicationArguments var1) throws Exception {
+    public void run(ApplicationArguments var1) {
         uploadProAuth();
         this.log.info("This will be execute when the project was started!");
     }
-
-
-
 
     /**
      * 收到上链事件
      */
     public void uploadProAuth() {
-
         Event event = new Event("RaffleCreated",
                 Arrays.<TypeReference<?>>asList(new TypeReference<Address>() {
                 }));
 
-
         ethFilter.addSingleTopic(EventEncoder.encode(event));
-
         log.info("启动监听RaffleCreated");
-
 
         web3j.ethLogFlowable(ethFilter).subscribe(log -> {
             List<Type> results = FunctionReturnDecoder.decode(log.getData(), event.getNonIndexedParameters());
